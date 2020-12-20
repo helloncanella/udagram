38b1115b12dd22864bb34e5ad16dae9c5fa56f84
Hellon Canella <helloncanella@gmail.com>
2020-12-19 21:35:38 -0300

    added some typescript types

--- src/server.ts
+++ src/server.ts
@@ -17,34 +17,37 @@ import { EventEmitter } from "events";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage", async (req, res) => {
-    const imageURL = req.query["image_url"] as string;
+  app.get(
+    "/filteredimage",
+    async (req: express.Request, res: express.Response) => {
+      const imageURL = req.query["image_url"] as string;
 
-    //    1. validate the image_url query
-    if (!isWebUri(imageURL))
-      return res.status(400).send(`${imageURL} is not a valid url`);
+      //    1. validate the image_url query
+      if (!isWebUri(imageURL))
+        return res.status(400).send(`${imageURL} is not a valid url`);
 
-    //    2. call filterImageFromURL(image_url) to filter the image
-    const filteredImageURL = await filterImageFromURL(imageURL);
-    const filePath = path.resolve(__dirname, filteredImageURL);
+      //    2. call filterImageFromURL(image_url) to filter the image
+      const filteredImageURL = await filterImageFromURL(imageURL);
+      const filePath = path.resolve(__dirname, filteredImageURL);
 
-    const emitter = new EventEmitter();
+      const emitter = new EventEmitter();
 
-    //    3. send the resulting file in the response
-    res.status(200).sendFile(filePath, () => {
-      emitter.emit("sent");
-    });
-
-    emitter.addListener("sent", () => {
-      //    4. deletes any files on the server on finish of the response
-      fs.unlink(filePath, (err) => {
-        if (!err) return;
-        return console.error(err);
+      //    3. send the resulting file in the response
+      res.status(200).sendFile(filePath, () => {
+        emitter.emit("sent");
       });
-    });
-  });
 
-  app.get("/", async (__, res) => {
+      emitter.addListener("sent", () => {
+        //    4. deletes any files on the server on finish of the response
+        fs.unlink(filePath, (err: NodeJS.ErrnoException) => {
+          if (!err) return;
+          return console.error(err);
+        });
+      });
+    }
+  );
+
+  app.get("/", async (__, res: express.Response) => {
     res.send("try GET /filteredimage?image_url={{}}");
   });
 
